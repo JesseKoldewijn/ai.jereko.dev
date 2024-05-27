fb485c154d9c9e7a44ceadf48de55dd62212317b
JesseKoldewijn <dev@glitchtech.eu>
2024-05-27 22:40:06 +0200

    init: codebase creation

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { GeistMono as geistMono } from "geist/font/mono";
+import { GeistSans as geistSans } from "geist/font/sans";
+
+import "./globals.css";
+import { cn } from "~/lib/utils";
+
+export const metadata: Metadata = {
+	title: {
+		absolute: "AI Jereko",
+		template: "%s | AI Jereko",
+	},
+	description:
+		"AI Jereko - A simple WebUI for interfacing with Ollama's AI models",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" className="dark">
+			<body className={cn(geistSans.variable, geistMono.variable)}>
+				{children}
+			</body>
+		</html>
+	);
+}
